07bdbd66f5e42faca4e16e955e20406874460296
Alex <Alex@Alex-PC>
2013-10-12 14:04:42 +0300

    Add tests

---
+++ tests/example.js
@@ -0,0 +1,54 @@
+/**
+ *
+ *  @revision    $Id: example.js 2013-10-08 04:54:16 Aleksey $
+ *  @created     2013-10-08 04:54:16
+ *  @category    Express Helpers
+ *  @package     connect-caminte
+ *  @version     0.0.1
+ *  @copyright   Copyright (c) 2009-2013 - All rights reserved.
+ *  @license     MIT License
+ *  @author      Alexey Gordeyev IK <aleksej@gordejev.lv>
+ *  @link        http://www.gordejev.lv/
+ *
+ *  Created by init script
+ *  App based on TrinteJS MVC framework
+ *  TrinteJS homepage http://www.trintejs.com
+ */
+
+var express = require('express'),
+        CaminteStore = require("./../lib/connect-caminte")(express),
+        app = express();
+
+
+app.use(express.cookieParser());
+app.use(express.session({
+    cookie: {
+        maxAge: 180000, // 3 min
+        secret: "feb722690aeccfa92ca9ee6fdf06e55a",
+    },
+    secret: "Wild Express-CaminteJS",
+    store: new CaminteStore({
+        driver: 'sqlite3',
+        collection: 'session',
+        db: {
+            database: "./db/session.db"
+        },
+        clear_interval: 60, // 1 min
+        secret: "feb722690aeccfa92ca9ee6fdf06e55a",
+        maxAge: 300000 // 3 min
+    })
+}));
+
+app.get('/', function(req, res, next) {
+    var sess = req.session;
+    if (sess.views) {
+        res.send('<p>views: ' + sess.views + '</p>'
+            + '<p>expires in: ' + (sess.cookie.maxAge / 1000) + 's</p>');
+        sess.views++;
+    } else {
+        sess.views = 1;
+        res.send('welcome to the session demo. refresh!');
+    }
+});
+
+app.listen(3500);
--- tests/test.js
+++ tests/test.js
@@ -3,7 +3,7 @@
  *  @revision    $Id: test.js 2013-10-08 04:54:16 Aleksey $
  *  @created     2013-10-08 04:54:16
  *  @category    Express Helpers
- *  @package     express-caminte
+ *  @package     connect-caminte
  *  @version     0.0.1
  *  @copyright   Copyright (c) 2009-2013 - All rights reserved.
  *  @license     MIT License
@@ -15,44 +15,66 @@
  *  TrinteJS homepage http://www.trintejs.com
  */
 
-var express = require('express'),
-        CaminteStore = require("./../lib/connect-caminte")(express),
-        app = express();
-
-
-app.use(express.cookieParser());
-app.use(express.session({
-    cookie: {
-        maxAge: 180000, // 3 min
-        secret: "feb722690aeccfa92ca9ee6fdf06e55a",
-    },
-    secret: "Wild Express-CaminteJS",
-    store: new CaminteStore({
-        driver: 'sqlite3',
-        collection: 'session',
-        db: {
-        //    host: "127.0.0.1",
-        //    port: "27017",
-        //    username: "test",
-        //    password: "2NDcVLWCTxhfXa9CA",
-            database: "./db/session.db"
-        },
-        clear_interval: 60, // 1 min
-        secret: "feb722690aeccfa92ca9ee6fdf06e55a",
-        maxAge: 300000 // 3 min
-    })
-}));
-
-app.get('/', function(req, res, next) {
-    var sess = req.session;
-    if (sess.views) {
-        res.send('<p>views: ' + sess.views + '</p>'
-            + '<p>expires in: ' + (sess.cookie.maxAge / 1000) + 's</p>');
-        sess.views++;
-    } else {
-        sess.views = 1;
-        res.send('welcome to the session demo. refresh!');
-    }
+var express = require('express');
+var CaminteStore = require("./../lib/connect-caminte")(express);
+var sid = 'uyv6r4djvbu7ubi8708uyuby' + new Date().getTime();
+var maxAge = 300000; // 3 min
+var store = new CaminteStore({
+    driver: 'memory',
+    collection: 'session',
+    db: {},
+    secret: "feb722690aeccfa92ca9ee6fdf06e55a",
+    maxAge: maxAge
 });
 
-app.listen(3500);
+exports['Create and store session '] = function(test) {
+    store.set(sid, {
+        cookie: {ssid: sid, maxAge: maxAge}
+    }, function(err, session) {
+        test.equal(err, null);
+        test.ok(session.sid, sid);
+        test.ok(session.session_data.cookie.ssid, sid);
+        test.equal(session.session_data.cookie.maxAge, maxAge);
+        test.equal(session.expireAfterSeconds, maxAge / 1000);
+        test.ok(session.ip, 'localhost');
+        test.ok(session.user, 'guest');
+        test.equal(session.logedIn, 0);
+        test.done();
+    });
+};
+
+exports['Find stored session '] = function(test) {
+    store.get(sid, function(err, session) {
+        test.equal(err, null);
+        test.ok(session.cookie.ssid, sid);
+        test.done();
+    });
+};
+
+exports['Count sessions '] = function(test) {
+    store.length(function(err, count) {
+        test.equal(err, null);
+        test.ok(typeof count, 'number');
+        test.equal(count, 1);
+        test.done();
+    });
+};
+
+exports['Select all sessions '] = function(test) {
+    store.all(function(err, sessions) {
+        test.equal(err, null);
+        test.equal(sessions.length, 1);
+        test.done();
+    });
+};
+
+exports['Destroy session '] = function(test) {
+    store.destroy(sid, function(err) {
+        test.equal(err, null);
+        store.get(sid, function(err, session) {
+            test.equal(err, null);
+            test.equal(session, null);
+            test.done();
+        });
+    });
+};
